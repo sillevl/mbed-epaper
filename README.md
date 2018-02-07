d0ebb0b551efaab4dcd8fe26942d9b3f1c14b78b
Sille Van Landschoot <info@sillevl.be>
2018-02-07 19:52:59 +0100

    update correct files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mbed-epaper
 mbed library for Good Display E-paper (like GDEW042Z15)
+
+
+referenced on the https://github.com/soonuse/gdew042z15_4.2inch_e-paper_b libraries
--- epaper/edp.h
+++ epaper/edp.h
@@ -5,15 +5,18 @@
 class Edp
 {
     public: 
-    Edp(SPI* spi, PinName cs_pin, PinName reset_pin, PinName dc_pin, PinName bussy_pin);
+    Edp(SPI* spi, PinName cs_pin, PinName reset_pin, PinName dc_pin, PinName busy_pin);
 
     protected:
     void sendCommand(unsigned char command);
     void sendData(unsigned char data);
+
+    void waitUntilIdle();
+    void reset();
     
     DigitalOut dc;
     DigitalOut cs;
     DigitalOut reset;
-    DigitalIn bussy;
+    DigitalIn busy;
     SPI* spi;
 };
