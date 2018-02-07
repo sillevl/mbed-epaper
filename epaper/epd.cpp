b946d31d40045c6d2bdfed6479b19ed53a7fa917
Sille Van Landschoot <info@sillevl.be>
2018-02-07 20:41:13 +0100

    improve initialisation of epd

--- epaper/epd.cpp
+++ epaper/epd.cpp
@@ -3,19 +3,41 @@
 Epd::Epd(SPI& spi_object, PinName cs_pin, PinName reset_pin, PinName dc_pin, PinName busy_pin):
  spi(spi_object), cs(cs_pin), display_reset(reset_pin), dc(dc_pin), busy(busy_pin)
 {
-    this->spi = spi;
+    spi.format(8, 0);
+    spi.frequency(2000000);
+}
+
+int Epd::init()
+{
+    // if (IfInit() != 0) {
+    //     return -1;
+    // }
+    reset();
+    sendCommand(BOOSTER_SOFT_START);
+    sendData(0x17);
+    sendData(0x17);
+    sendData(0x17);
+    sendCommand(POWER_ON);
+    waitUntilIdle();
+    sendCommand(PANEL_SETTING);
+    sendData(0x0F);
+    return 0;
 }
 
 void Epd::sendCommand(unsigned char command)
 {
     dc = 0;
+    cs = 0;
     //spi->transfer(command);
+    cs = 1;
 }
 
 void Epd::sendData(unsigned char data)
 {
     dc = 1;
+    cs = 0;
     //spi->transfer(data);
+    cs = 1;
 }
 
 void Epd::waitUntilIdle()
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
  
 #include "epd.h"
 
+Serial pc(SERIAL_TX, SERIAL_RX);
+
 DigitalOut myled(LED1);
 SPI spi(D11, D12, D13); // mosi, miso, sclk
 // DigitalOut cs(D0);
@@ -9,6 +11,11 @@ SPI spi(D11, D12, D13); // mosi, miso, sclk
 Epd epd(spi, D3, D2, D1, D0); // spi, cs, reset, dc, busy
  
 int main() {
+    pc.printf("\r\n** e-Paper demo **\r\n")
+    if (epd.init() != 0) {
+        pc.printf("e-Paper init failed\r\n");
+        return;
+    }
 
 
 
